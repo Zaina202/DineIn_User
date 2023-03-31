44eada86c18762440d3ab59f4d8ceca5e34e4212
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2023-03-31 16:40:29 +0300

    skeleton

---
+++ Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿//using Dinein_UserApp.Services;
+using Dinein_UserApp.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Dinein_UserApp
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+           // DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
