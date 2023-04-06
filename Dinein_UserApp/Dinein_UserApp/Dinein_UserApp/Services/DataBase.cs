a8038e5d59c1f412a1bfb4973aca0fcc3579a614
deemashahawan <dshahwan42@gmail.com>
2023-04-06 00:52:16 -0700

    display meals data from data base

--- Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/Services/DataBase.cs
+++ Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/Services/DataBase.cs
@@ -5,6 +5,8 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
+using Xamarin.Forms;
+using Menu = Dinein_UserApp.Models.Menu;
 
 namespace Dinein_UserApp.Services
 {
@@ -29,6 +31,23 @@ namespace Dinein_UserApp.Services
 
                 return false;
             }
+        }
+
+        public async Task<List<Menu>> GetAll()
+        {
+            var MenuList = new List<Menu>();
+            var menu = await fc.Child(nameof(Menu)).OnceAsync<Menu>();
+            foreach (var item in menu)
+            {
+                var Meal = new Menu()
+                {
+                    Name = item.Object.Name,
+                    Price = item.Object.Price,
+                    Image=item.Object.Image,
+                };
+                MenuList.Add(Meal);
+            }
+            return MenuList;
         }
     }
 }
--- Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/ViewModels/MenuViewModel.cs
+++ Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/ViewModels/MenuViewModel.cs
@@ -1,10 +1,43 @@
-﻿using System;
+﻿using Dinein_UserApp.Models;
+using Dinein_UserApp.Services;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
+using Xamarin.Forms;
 
 namespace Dinein_UserApp.ViewModels
 {
-    class MenuViewModel
+    class MenuViewModel : INotifyPropertyChanged
     {
+        private  DataBase dataBase;
+
+        private List<Models.Menu> _menuItems;
+        public List<Models.Menu> MenuItems
+        {
+            get { return _menuItems; }
+            set
+            {
+                _menuItems = value;
+                OnPropertyChanged(nameof(MenuItems));
+            }
+        }
+
+        public MenuViewModel()
+        {
+            dataBase = new DataBase();
+            LoadMenuItems();
+        }
+
+        private async void LoadMenuItems()
+        {
+            MenuItems = await dataBase.GetAll();
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
