7ea8d90bab46b05868b8d0740644ff7fe70a456b
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2023-04-10 17:58:37 +0300

    SignUp logic

--- Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/Models/User.cs
+++ Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/Models/User.cs
@@ -6,7 +6,9 @@ namespace Dinein_UserApp.Models
 {
     public class User
     {
+        public string UserId { get; set; }
         public string Username { get; set; }
+        public string Email { get; set; }
         public string PhoneNumber { get; set; }
         public string Password { get; set; }
         private string confirmPassword { get; set; }
--- Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/ViewModels/SignupViewModel.cs
+++ Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/ViewModels/SignupViewModel.cs
@@ -5,13 +5,16 @@ using MvvmHelpers;
 using System;
 using System.Security.Cryptography;
 using System.Text;
+using System.Text.RegularExpressions;
 using Xamarin.Forms;
+
 
 namespace Dinein_UserApp.ViewModels
 {
     public class SignupViewModel : BaseViewModel
     {
         private string username;
+        private string email;
         private string phoneNumber;
         private string password;
         private string confirmPassword;
@@ -21,7 +24,11 @@ namespace Dinein_UserApp.ViewModels
             get => username;
             set => SetProperty(ref username, value);
         }
-
+        public string Email
+        {
+            get => email;
+            set => SetProperty(ref email, value);
+        }
         public string PhoneNumber
         {
             get => phoneNumber;
@@ -41,18 +48,38 @@ namespace Dinein_UserApp.ViewModels
         }
         private bool IsValidPhoneNumber(string phoneNumber)
         {
-            // TODO: implement phone number validation logic
-            // This can be done using regular expressions or third-party libraries
+            if (string.IsNullOrEmpty(phoneNumber))
+            {
+                return false;
+            }
 
-            return true; // For now, we will just assume that all phone numbers are valid
+            // use a regular expression to validate the phone number
+            var regex = new Regex(@"^05\d([-]{0,1})\d{7}$");
+            return regex.IsMatch(phoneNumber);// For now, we will just assume that all phone numbers are valid
         }
-
+   
         public Command SignUpCommand => new Command(async () =>
         {
+            if (string.IsNullOrWhiteSpace(Username) || string.IsNullOrWhiteSpace(PhoneNumber) || string.IsNullOrWhiteSpace(Email) || string.IsNullOrWhiteSpace(Password) || string.IsNullOrWhiteSpace(ConfirmPassword))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Please fill in all required fields.", "OK");
+                return;
+            }
+
             // check if password and confirm password match
             if (Password != ConfirmPassword)
             {
                 await Application.Current.MainPage.DisplayAlert("Error", "Passwords do not match.", "OK");
+                return;
+            }
+            if (!IsValidPhoneNumber(PhoneNumber))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Invalid phone number.", "OK");
+                return;
+            }
+            if (!IsValidEmail(Email))
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Invalid email.", "OK");
                 return;
             }
 
@@ -60,19 +87,36 @@ namespace Dinein_UserApp.ViewModels
             var newUser = new User
             {
                 Username = Username,
+                Email = Email,
                 PhoneNumber = PhoneNumber,
                 Password = HashPassword(Password) // encrypt the password using a cryptographic hash function
             };
-
             // get firebase database reference
             var firebase = new FirebaseClient("https://dine-in-54308-default-rtdb.firebaseio.com/");
 
+            // check if phone number is already used
+            var existingUser = await firebase.Child("Users").OrderBy("PhoneNumber").EqualTo(PhoneNumber).OnceAsync<User>();
+            if (existingUser.Count > 0)
+            {
+                await Application.Current.MainPage.DisplayAlert("Error", "Phone number is already taken.", "OK");
+                return;
+            }
             // create new user node in database and set value to user object
             await firebase.Child("Users").PostAsync(newUser);
 
             // display success message
             await Application.Current.MainPage.DisplayAlert("Success", "User account created successfully!", "OK");
         });
+        public bool IsValidEmail(string email)
+        {
+            string pattern = @"^[^@\s]+@[^@\s]+\.[^@\s]+$";
+            Regex regex = new Regex(pattern);
+
+            Match match = regex.Match(email);
+
+            return match.Success;
+
+        }
 
         private string HashPassword(string password)
         {
@@ -83,5 +127,6 @@ namespace Dinein_UserApp.ViewModels
                 return hashedString;
             }
         }
+      
     }
 }
