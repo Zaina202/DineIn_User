ac10bb339adabce0c41833e5bf2faabfbc405204
Zaina202 <92650788+Zaina202@users.noreply.github.com>
2023-04-17 01:11:50 +0300

    edit on design

--- Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/ViewModels/CurrentViewModel.cs
+++ Dinein_UserApp/Dinein_UserApp/Dinein_UserApp/ViewModels/CurrentViewModel.cs
@@ -7,27 +7,7 @@ namespace Dinein_UserApp.ViewModels
 {
     class CurrentViewModel : INotifyPropertyChanged
     {
-        private int _editButtonCornerRadius = 20;
-        public int EditButtonCornerRadius
-        {
-            get => _editButtonCornerRadius;
-            set
-            {
-                _editButtonCornerRadius = value;
-                OnPropertyChanged(nameof(EditButtonCornerRadius));
-            }
-        }
-
-        private int _cancelButtonCornerRadius = 20;
-        public int CancelButtonCornerRadius
-        {
-            get => _cancelButtonCornerRadius;
-            set
-            {
-                _cancelButtonCornerRadius = value;
-                OnPropertyChanged(nameof(CancelButtonCornerRadius));
-            }
-        }
+       
 
         public event PropertyChangedEventHandler PropertyChanged;
         protected virtual void OnPropertyChanged(string propertyName)
